9ad93c8f59ccf27b94ce8572f2f53b5f9179da56
Ahfu C Kit III <196157628+ahfuckit@users.noreply.github.com>
2026-02-09 02:07:53 -0700

    Add files via upload
    Signed-off-by: Ahfu C Kit III <196157628+ahfuckit@users.noreply.github.com>

---
+++ maybe_tampered/JavaScript.js
@@ -0,0 +1,18 @@
+```JavaScript
+
+const settings = {
+console: console || {},
+that: this
+}
+function start(settings = settings) {
+class InitLogs {
+  constructor() {
+  const inject = this;
+  Object.assign(this, inject, Window);
+this.inject = settings;
+}
+
+const init = () => {return new InitLogs()};
+init();
+}})()
+```
